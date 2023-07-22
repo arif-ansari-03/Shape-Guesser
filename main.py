2ac50000648e953640f1e9f004df9e2939943834
arif-ansari-03 <arifansari010603@gmail.com>
2023-07-22 18:38:50 +0530

    begin

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 360))
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("black")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
