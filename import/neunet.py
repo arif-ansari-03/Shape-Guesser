893e6ad1be8403dde1bb0c0b30b44cb1f8e40660
arif-ansari-03 <arifansari010603@gmail.com>
2023-07-24 21:46:48 +0530

    defined layer and parameter class

--- import/neunet.py
+++ import/neunet.py
@@ -1,3 +1,16 @@
+class Param:
+    def __init__(self, matrix, bias):
+        self.weights = matrix
+        self.biases = bias
 
-class Layer:
     
+class Layer:
+
+    def __init__(self, column, param):
+        self.column = column
+        self.param = param
+
+    def print(self):
+        print(self.layer)
+        print("\n")
+        print(self.param.weights + "\n" + self.param.bias)
