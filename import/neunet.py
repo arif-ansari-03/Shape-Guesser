211b7cf65e80337c298f65fdcd2f2f2efdc2b518
arif-ansari-03 <arifansari010603@gmail.com>
2023-08-19 18:57:46 +0530

    set up neunet.py

--- import/neunet.py
+++ import/neunet.py
@@ -1,16 +1,33 @@
-class Param:
-    def __init__(self, matrix, bias):
-        self.weights = matrix
-        self.biases = bias
+import numpy
 
-    
 class Layer:
 
-    def __init__(self, column, param):
-        self.column = column
-        self.param = param
+    def __init__(self, Num_nodes, Num_in_nodes, Weights, Biases):
+        self.numNodes = Num_nodes               #number of nodes in output
+        self.numInNodes = Num_in_nodes          #number of incoming nodes
+        self.weights = Weights
+        self.biases = Biases
 
-    def print(self):
-        print(self.layer)
-        print("\n")
-        print(self.param.weights + "\n" + self.param.bias)
+    def calc_out( inLayer ):
+        outLayer = numpy.matmul(Layer.Weights, inLayer)
+        for i in range(Layer.numNodes):
+            outLayer[i] += Layer.Biases[i]
+            outLayer[i] = 1/(1 + numpy.exp(-outLayer[i]))
+
+        return outLayer
+    
+class Network:
+
+    def __init__(self, NumLayers, Layers):
+        self.numLayers = NumLayers
+        self.layers = Layers
+
+    def calc_out( inArray ):
+        outArray = list(inArray)
+        for i in range(Network.numLayers):
+            outArray = Network.layers[i].calc_out( outArray )
+
+        return outArray             # The final output of the neural network
+    
+
+
