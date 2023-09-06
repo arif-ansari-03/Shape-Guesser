6723eec69b950a475a0a3d48d07322072e3bdee2
arif-ansari-03 <arifansari010603@gmail.com>
2023-09-06 22:55:58 +0530

    upd

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 import neunet
+import numpy
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 360))
+h = 100
+w = 200
+grid = [[(255, 0, 0, 0) for x in range(w)] for y in range(h)]
+
+screen = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
 
 
@@ -12,7 +17,9 @@ running = True
 screen.fill("black")
 
 while running:
-
+    for i in range(h):
+        for j in range(w):
+            pygame.draw.rect(screen, i * 255 + j * 255, pygame.Rect(i, j, i+5, j+5))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
